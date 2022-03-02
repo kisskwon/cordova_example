4f291b434fc5237a4ae307e41e3f9e15000bc501
kiss.kwon <kiss.kwon@lge.com>
2022-03-03 00:50:31 +0900

    merge react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  const [inputText, setInputText] = useState();
+
+  console.log("kks", window);
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+  const callCordova = () => {
+    window.cordova.plugins.Syncup.coolMethod(inputText, (result) => {
+      console.log("kks", result);
+    });
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <input value={inputText} onChange={onChange} />
+        <button onClick={callCordova}>SEND</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
